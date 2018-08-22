ff628ee4b6fd961676bc3a00163a2409ecd45946
Hwang kyujin <https://github.com/HWANGKYUJIN>
2018-08-22 23:34:52 +0900

    for using font

---
+++ app/src/main/java/com/fastbooster/android_teamwith/JoinedTeamActivity.java
@@ -0,0 +1,23 @@
+package com.fastbooster.android_teamwith;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+public class JoinedTeamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joined_team);
+
+        ListView joinedTeamListView=(ListView)findViewById(R.id.joinedTeamListView);
+
+
+
+        //어씽크 태스크 생성 및 호출
+            //어씽크 태스크에서 Api 호출, 받은 데이터로 어댑터 생성
+
+        //리스트뷰에 어댑터 세팅하면 끝!
+    }
+}
