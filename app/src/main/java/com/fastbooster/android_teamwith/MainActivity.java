4079862ef4fc9199904e18097456988244ec445a
kyungjunyim <killop2000@gmail.com>
2018-08-22 16:52:48 +0900

    0822 16:52 jun

--- app/src/main/java/com/fastbooster/android_teamwith/MainActivity.java
+++ app/src/main/java/com/fastbooster/android_teamwith/MainActivity.java
@@ -1,14 +1,15 @@
 package com.fastbooster.android_teamwith;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // 안녕하세요?
     }
 }
