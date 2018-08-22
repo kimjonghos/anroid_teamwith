3dbfd8c5340c36acf270b0d306deb9f78ffd31d4
kyungjunyim <killop2000@gmail.com>
2018-08-22 21:03:01 +0900

    0822 21:02 jun

--- app/src/main/java/com/fastbooster/android_teamwith/LoginActivity.java
+++ app/src/main/java/com/fastbooster/android_teamwith/LoginActivity.java
@@ -1,14 +1,25 @@
 package com.fastbooster.android_teamwith;
 
 import android.app.Activity;
+import android.graphics.Typeface;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_login);
+
+        EditText editText = findViewById(R.id.editText);
+        TextView textView = findViewById(R.id.textView11);
+        Typeface typeface = ResourcesCompat.getFont(this, R.font.font_bmjua);
+        editText.setTypeface(typeface);
+        textView.setTypeface(typeface);
     }
 }
