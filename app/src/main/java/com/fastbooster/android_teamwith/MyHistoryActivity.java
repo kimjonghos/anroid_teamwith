3916cbf41e549d2699909260a2d2448da3c69612
kyungjunyim <killop2000@gmail.com>
2018-08-22 21:32:48 +0900

    0822 21:32 jun

---
+++ app/src/main/java/com/fastbooster/android_teamwith/MyHistoryActivity.java
@@ -0,0 +1,13 @@
+package com.fastbooster.android_teamwith;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_history);
+    }
+}
