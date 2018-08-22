80b50a014fbb3577573c5fbb859fcc098490b4cc
kyungjunyim <killop2000@gmail.com>
2018-08-22 22:34:58 +0900

    0822 22:34 jun

--- app/src/main/java/com/fastbooster/android_teamwith/MyHistoryActivity.java
+++ app/src/main/java/com/fastbooster/android_teamwith/MyHistoryActivity.java
@@ -1,9 +1,10 @@
 package com.fastbooster.android_teamwith;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MyHistoryActivity extends AppCompatActivity {
+public class MyHistoryActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
