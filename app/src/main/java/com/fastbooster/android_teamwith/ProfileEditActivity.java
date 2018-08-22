487bf743fcf8868a518efda7e4128e95bf471fb4
lana <ygj02054@gmail.com>
2018-08-22 20:20:16 +0900

    member intro

--- app/src/main/java/com/fastbooster/android_teamwith/ProfileEditActivity.java
+++ app/src/main/java/com/fastbooster/android_teamwith/ProfileEditActivity.java
@@ -1,14 +1,26 @@
 package com.fastbooster.android_teamwith;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
-public class ProfileEditActivity extends AppCompatActivity {
+public class ProfileEditActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_edit);
+        LinearLayout introLayout = findViewById(R.id.memberIntroLayout);
 
+        introLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfileEditActivity.this,MemberIntroActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
