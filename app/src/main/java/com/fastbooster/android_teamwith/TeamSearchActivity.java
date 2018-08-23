23c3843c79a2ef775d22e4fca5d9c5fbc0af55b4
lana <ygj02054@gmail.com>
2018-08-23 11:42:45 +0900

    teamsearch

--- app/src/main/java/com/fastbooster/android_teamwith/TeamSearchActivity.java
+++ app/src/main/java/com/fastbooster/android_teamwith/TeamSearchActivity.java
@@ -1,9 +1,10 @@
 package com.fastbooster.android_teamwith;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class TeamSearchActivity extends AppCompatActivity {
+public class TeamSearchActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
