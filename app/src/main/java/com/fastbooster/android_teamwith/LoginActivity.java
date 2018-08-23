48fc9afeb936fb25ac023f93f73a60efb91aee5d
kyungjunyim <killop2000@gmail.com>
2018-08-23 17:04:59 +0900

    0823 17:04 jun

--- app/src/main/java/com/fastbooster/android_teamwith/LoginActivity.java
+++ app/src/main/java/com/fastbooster/android_teamwith/LoginActivity.java
@@ -1,24 +1,13 @@
 package com.fastbooster.android_teamwith;
 
 import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import com.fastbooster.android_teamwith.task.LoginTask;
 
 public class LoginActivity extends Activity {
     private EditText editTextId;
@@ -34,14 +23,13 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         init();
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try {
-                    login(view);
-                } catch(Exception e) {
-                    e.printStackTrace();
-                }
+                LoginTask loginTask = new LoginTask(getApplicationContext());
+                String[] accountInfos = new String[]{editTextId.getText().toString(), editTextPwd.getText().toString()};
+                loginTask.execute(accountInfos);
             }
         });
     }
@@ -55,76 +43,4 @@ public class LoginActivity extends Activity {
         btnRegister = this.findViewById(R.id.y_btn_register);
         btnEnter = this.findViewById(R.id.y_btn_enter);
     }
-
-
-    public void login(View view) {
-        InputStream is = null;
-        String result = "";
-        try {
-            URL urlCon = new URL("http://192.168.30.7:8089/api/login");
-            HttpURLConnection httpCon = (HttpURLConnection)urlCon.openConnection();
-
-            String json = "";
-
-            // build jsonObject
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.accumulate("memberId", editTextId.getText());
-            jsonObject.accumulate("memberPassword", editTextPwd.getText());
-
-            // convert JSONObject to JSON to String
-            json = jsonObject.toString();
-
-            // ** Alternative way to convert Person object to JSON string usin Jackson Lib
-            // ObjectMapper mapper = new ObjectMapper();
-            // json = mapper.writeValueAsString(person);
-
-            // Set some headers to inform server about the type of the content
-            httpCon.setRequestProperty("Accept", "application/json");
-            httpCon.setRequestProperty("Content-type", "application/json");
-
-            // OutputStream으로 POST 데이터를 넘겨주겠다는 옵션.
-            httpCon.setDoOutput(true);
-            // InputStream으로 서버로 부터 응답을 받겠다는 옵션.
-            httpCon.setDoInput(true);
-
-            OutputStream os = httpCon.getOutputStream();
-            os.write(json.getBytes("euc-kr"));
-            os.flush();
-            // receive response as inputStream
-            try {
-                is = httpCon.getInputStream();
-                // convert inputstream to string
-                if(is != null)
-                    result = convertInputStreamToString(is);
-                else
-                    result = "Did not work!";
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-            finally {
-                httpCon.disconnect();
-            }
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
-    }
-
-    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
-        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
-        String line = "";
-        String result = "";
-        while((line = bufferedReader.readLine()) != null)
-            result += line;
-
-        inputStream.close();
-        return result;
-    }
-
 }
