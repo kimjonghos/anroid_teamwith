e6a2a6d844dabe4d8a29487a2f5ff693e48fce17
kimjonghos <olikuj933@gmail.com>
2018-08-26 10:09:21 +0900

    portfolio

--- app/src/main/java/com/fastbooster/android_teamwith/task/PortfolioDetailTask.java
+++ app/src/main/java/com/fastbooster/android_teamwith/task/PortfolioDetailTask.java
@@ -3,14 +3,25 @@ package com.fastbooster.android_teamwith.task;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.fastbooster.android_teamwith.PortfolioActivity;
 import com.fastbooster.android_teamwith.R;
+import com.fastbooster.android_teamwith.adapter.PortfolioContentAdapter;
 import com.fastbooster.android_teamwith.api.PortfolioApi;
+import com.fastbooster.android_teamwith.model.PortfolioContentVO;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
 public class PortfolioDetailTask extends AsyncTask<String, Void, JSONObject> {
     private final Context context;
@@ -29,7 +40,9 @@ public class PortfolioDetailTask extends AsyncTask<String, Void, JSONObject> {
     protected void onPostExecute(JSONObject portfolioSimpleVOS) {
 
         try {
-            String r = portfolioSimpleVOS.getString("portfolioId");
+            JSONObject ps=portfolioSimpleVOS.getJSONObject("portfolio");
+            JSONArray pary=portfolioSimpleVOS.getJSONArray("portfolioContent");
+            String r = ps.getString("portfolioId");
 
             PortfolioActivity view = (PortfolioActivity) context;
             TextView tvTitle = (TextView) view.findViewById(R.id.k_tv_title);
@@ -42,18 +55,31 @@ public class PortfolioDetailTask extends AsyncTask<String, Void, JSONObject> {
             TextView tvRole = (TextView) view.findViewById(R.id.k_tv_role);
             TextView tvWork = (TextView) view.findViewById(R.id.k_tv_work);
             ImageView ivPortfolioPic = (ImageView) view.findViewById(R.id.k_ivPortfolioPic);
-            tvTitle.setText(portfolioSimpleVOS.getString("portfolioTitle"));
-            tvCategory.setText(portfolioSimpleVOS.getString("projectCategoryId"));
-            tvIntro.setText(portfolioSimpleVOS.getString("portfolioIntro"));
-            tvPeopleNum.setText(portfolioSimpleVOS.getString("portfolioPeopleNum"));
-            tvTeamName.setText(portfolioSimpleVOS.getString("portfolioTeamName"));
-            tvStartDate.setText(portfolioSimpleVOS.getString("portfolioStartDate"));
-            tvEndDate.setText(portfolioSimpleVOS.getString("portfolioEndDate"));
-            tvRole.setText(portfolioSimpleVOS.getString("portfolioRole"));
-            tvWork.setText(portfolioSimpleVOS.getString("portfolioWork"));
-            ivPortfolioPic.setTag(portfolioSimpleVOS.get("portfolioPic"));
+            tvTitle.setText(ps.getString("portfolioTitle"));
+            tvCategory.setText(ps.getString("projectCategoryId"));
+            tvIntro.setText(ps.getString("portfolioIntro"));
+            tvPeopleNum.setText(ps.getString("portfolioPeopleNum"));
+            tvTeamName.setText(ps.getString("portfolioTeamName"));
+            tvStartDate.setText(ps.getString("portfolioStartDate"));
+            tvEndDate.setText(ps.getString("portfolioEndDate"));
+            tvRole.setText(ps.getString("portfolioRole"));
+            tvWork.setText(ps.getString("portfolioWork"));
+            ivPortfolioPic.setTag(ps.get("portfolioPic"));
             ImageTask imgTask = new ImageTask(context);
             imgTask.execute(ivPortfolioPic);
+            List<PortfolioContentVO>  portfolioContenList=new ArrayList<>();
+            for(int i=0;i<pary.length();i++){
+                JSONObject j=pary.getJSONObject(i);
+                portfolioContenList.add(new PortfolioContentVO(j));
+
+            }
+            PortfolioContentAdapter pca=new PortfolioContentAdapter(context,portfolioContenList);
+            GridView g=(GridView)view.findViewById(R.id.k_portfolioContent);
+            g.setAdapter(pca);
+//            task(view,portfolioContenList);
+
+
+
 
 
         } catch (Exception e) {
@@ -67,14 +93,30 @@ public class PortfolioDetailTask extends AsyncTask<String, Void, JSONObject> {
         try{
             Log.e("PortfolioDetailTask - onProgressUpdate call...",strings[0]);
             JSONObject o=PortfolioApi.getPortfolioDetail(strings[0]);
-            JSONObject ps=o.getJSONObject("portfolio");
+//            JSONObject ps=o.getJSONObject("portfolio");
 
-            return ps;
+
+
+            return o;
         }catch (Exception e){
             e.printStackTrace();
         }
         return null;
     }
+//    public void task( PortfolioActivity view,List<PortfolioContentVO> list){
+//        GridView g=(GridView)view.findViewById(R.id.k_portfolioContent);
+//        for(int i=0;i<list.size();i++){
+//            if(list.get(i).getPortfolioContentName().equals("image")){
+//                ImageView iv=new ImageView(context);
+//                iv.setLayoutParams(new ViewGroup.LayoutParams(WRAP_CONTENT, WRAP_CONTENT));
+//                iv.setTag(list.get(i).getPortfolioContentValue());
+//                ImageTask imageViewTask= new ImageTask(context);
+//                imageViewTask.execute(iv);
+//                g.add
+//            }
+//        }
+//
+//    }
 }
 
 
