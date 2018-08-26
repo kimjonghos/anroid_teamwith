cabd199e25760bca9cdfaf70c3ca431714bb4829
lana <ygj02054@gmail.com>
2018-08-26 12:42:26 +0900

    recent portfolio

--- app/src/main/java/com/fastbooster/android_teamwith/task/PortfolioSearchTask.java
+++ app/src/main/java/com/fastbooster/android_teamwith/task/PortfolioSearchTask.java
@@ -2,6 +2,7 @@ package com.fastbooster.android_teamwith.task;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.GridView;
@@ -9,10 +10,15 @@ import android.widget.GridView;
 import com.fastbooster.android_teamwith.R;
 import com.fastbooster.android_teamwith.SearchActivity;
 import com.fastbooster.android_teamwith.adapter.PologPortfoliListAdapter;
-import com.fastbooster.android_teamwith.api.PortfolioApi;
+import com.fastbooster.android_teamwith.api.ApiUtil;
 import com.fastbooster.android_teamwith.model.PortfolioSimpleVO;
 import com.fastbooster.android_teamwith.util.Criteria;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PortfolioSearchTask extends AsyncTask<Object, Void, List<PortfolioSimpleVO>> {
@@ -40,8 +46,35 @@ public class PortfolioSearchTask extends AsyncTask<Object, Void, List<PortfolioS
         //http url connection
         try {
             Log.v("cnt", condition.length + "");
-            return PortfolioApi.getPortfolioList((Criteria) condition[0], (List<String>) condition[1],
-                    (String) condition[2]);
+            Uri.Builder params = new Uri.Builder();
+            String keyword = (String) condition[2];
+            List<String> project = (List<String>) condition[1];
+            Criteria cri = (Criteria) condition[0];
+            if ((keyword == null || keyword.trim().equals("")) && project == null) {
+                params.appendPath("recent");
+            } else {
+                if (keyword != null) {
+                    params.appendQueryParameter("keyword", keyword);
+                }
+                if (project != null) {
+                    for (String p : project) {
+                        params.appendQueryParameter("project", p);
+                    }
+                }
+            }
+            if (cri != null) {
+                params.appendQueryParameter("page", cri.getPage() + "");
+                params.appendQueryParameter("perPageNum", cri.getPerPageNum() + "");
+            }
+            List<PortfolioSimpleVO> list = new ArrayList<>();
+            JSONObject obj = ApiUtil.getJsonObject("/portfolioSearch" + params.toString());
+            JSONArray array = obj.getJSONArray("portfolioList");
+            PortfolioSimpleVO portfolioSimpleVO = null;
+            for (int i = 0; i < array.length(); i++) {
+                list.add(new PortfolioSimpleVO(array.getJSONObject(i)));
+            }
+
+            return list;
         } catch (Exception e) {
             e.printStackTrace();
             Log.v(TAG, "포트폴리오 서치 태스크 getPortfolio 오류");
