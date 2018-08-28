5275cc3c0ab973b459076b2549942a4dfb0d8caf
lana <ygj02054@gmail.com>
2018-08-28 14:24:09 +0900

    added profile image update

--- app/src/main/java/com/fastbooster/android_teamwith/ProfileEditActivity.java
+++ app/src/main/java/com/fastbooster/android_teamwith/ProfileEditActivity.java
@@ -6,10 +6,17 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -22,12 +29,16 @@ import com.fastbooster.android_teamwith.share.ApplicationShare;
 import com.fastbooster.android_teamwith.task.ImageTask;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class ProfileEditActivity extends BarActivity {
     public static final int MEMBER_INTRO = 1;
+    private final int GALLERY_CODE = 1112;
+
     String[] roleKeyList; //역할의 키 값 목록
     String[] regionKeyList; //지역의 키값 목록
 
@@ -64,6 +75,13 @@ public class ProfileEditActivity extends BarActivity {
 
         mptask.execute();
 
+        memberPic.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                selectGallery();
+                return false;
+            }
+        });
 
         //저장하기 버튼 눌렀을 때
         profileEdit.setOnClickListener(new View.OnClickListener() {
@@ -282,16 +300,131 @@ public class ProfileEditActivity extends BarActivity {
 
     }
 
+    private void selectGallery() {
+
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setData(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/*");
+        startActivityForResult(intent, GALLERY_CODE);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == MEMBER_INTRO) {
-            if (resultCode == RESULT_OK) {
-                memberIntro = data.getStringExtra("memberIntro");
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+
+            switch (requestCode) {
+
+                case GALLERY_CODE:
+                    sendPicture(data.getData()); //갤러리에서 가져오기
+                    break;
+                case MEMBER_INTRO:
+                    memberIntro = data.getStringExtra("memberIntro");
+                    break;
+                default:
+                    Toast.makeText(this, "fail", Toast.LENGTH_SHORT).show();
+                    break;
             }
+
+        }
+    }
+
+
+    private void sendPicture(Uri imgUri) {
+
+        final String imagePath = getRealPathFromURI(imgUri); // path 경로
+        ExifInterface exif = null;
+        try {
+            exif = new ExifInterface(imagePath);
+            int exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+            int exifDegree = exifOrientationToDegrees(exifOrientation);
+            final Bitmap bitmap = resize(this, imgUri, 500);//이미지 리사이즈 후 경로를 통해 비트맵으로 전환
+
+            memberPic.setImageBitmap(rotate(bitmap, exifDegree));//이미지 뷰에 비트맵 넣기
+            try {
+                new Thread() {
+                    @Override
+                    public void run() {
+                        super.run();
+                        try {
+                            String sessionId = getSharedPreferences("memberPref", MODE_PRIVATE).getString("sessionId", "");
+                            ApiUtil.sendImage(sessionId, bitmap);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }.start();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
     }
 
+    private int exifOrientationToDegrees(int exifOrientation) {
+        if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
+            return 90;
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {
+            return 180;
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {
+            return 270;
+        }
+        return 0;
+    }
+
+    private Bitmap rotate(Bitmap src, float degree) {
+
+// Matrix 객체 생성
+        Matrix matrix = new Matrix();
+// 회전 각도 셋팅
+        matrix.postRotate(degree);
+// 이미지와 Matrix 를 셋팅해서 Bitmap 객체 생성
+        return Bitmap.createBitmap(src, 0, 0, src.getWidth(),
+                src.getHeight(), matrix, true);
+    }
+
+    private String getRealPathFromURI(Uri contentUri) {
+        int column_index = 0;
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
+        if (cursor.moveToFirst()) {
+            column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        }
+
+        return cursor.getString(column_index);
+    }
+
+    private Bitmap resize(Context context, Uri uri, int resize) {
+        Bitmap resizeBitmap = null;
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        try {
+            BitmapFactory.decodeStream(context.getContentResolver().openInputStream(uri), null, options); // 1번
+
+            int width = options.outWidth;
+            int height = options.outHeight;
+            int samplesize = 1;
+
+            while (true) {//2번
+                if (width / 2 < resize || height / 2 < resize)
+                    break;
+                width /= 2;
+                height /= 2;
+                samplesize *= 2;
+            }
+
+            options.inSampleSize = samplesize;
+            Bitmap bitmap = BitmapFactory.decodeStream(context.getContentResolver().openInputStream(uri), null, options); //3번
+            resizeBitmap = bitmap;
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return resizeBitmap;
+    }
     public class MyProfileTask extends AsyncTask<Void, Void, MemberVO> {
 
         static final String TAG = "member data...";
